ba9ce9d361f3fe364298b449a5625a5e3b9d703d
torz <torz@gmx.com>
2013-04-05 21:25:14 +0100

    2nd commit

---
+++ tc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import os
+import urwid
+
+class BrowserPanel(urwid.ListBox):
+    def __init__(self):
+
+
+class MainFrame(urwid.Frame):
+    def __init__():
+
+
+def main():
+
+if __name__ == '__main__':
+    main()
+
