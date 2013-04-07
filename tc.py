dc2d97cc1cc9decf46060eb10f039c3a8ebf8e0d
torz <torz@gmx.com>
2013-04-07 13:15:43 +0100

    2 panels with linebox but not working

--- tc.py
+++ tc.py
@@ -10,14 +10,14 @@ palette = [
     ('bg', 'black', 'dark blue'),]
 
 
-class BrowserPanel(urwid.ListBox):
+class BrowserPanel(urwid.LineBox):
     def __init__(self):
         self.rootPath = '/'
         self.previousPath = '/'
         self.currentPath = '/'
         self.fileList = sorted(os.listdir(self.currentPath))
-        body = self.create_buttons() 
-        super(BrowserPanel, self).__init__(body)
+        body = self.create_buttons()
+        super(BrowserPanel, self).__init__(body, self.currentPath)
 
     def update_file_list(self, choice):
         if choice == '..':
@@ -36,28 +36,26 @@ class BrowserPanel(urwid.ListBox):
             self.fileList.insert(0, '..')
 
     def create_buttons(self):
-        body = [urwid.Text(self.currentPath), urwid.Divider()]
+        body = []
         for oneFile in sorted(self.fileList):
             button = urwid.Button(oneFile)
             if os.path.isdir(os.path.join(self.currentPath, oneFile)):
                 urwid.connect_signal(button, 'click', self.update_body, oneFile)
             body.append(urwid.AttrMap(button, None, focus_map='reversed'))
-        return urwid.SimpleFocusListWalker(body)
+        return urwid.ListBox(urwid.SimpleFocusListWalker(body))
     
     def update_body(self, button, choice):
         self.update_file_list(choice)
-        self.body = self.create_buttons()
-        # loop.draw_screen()
-                
-#class MainFrame(urwid.Frame):
-#    def __init__():
+        self.original_widget = self.create_buttons()
 
 def exit_program(key):
     if key == 'q':
         raise urwid.ExitMainLoop()
 
 def main():
-    main = BrowserPanel()
+    panel1 = BrowserPanel()
+    panel2 = BrowserPanel()
+    main = urwid.Columns([panel1, panel2])
     loop = urwid.MainLoop(main, palette, unhandled_input=exit_program)
     loop.run()
 
